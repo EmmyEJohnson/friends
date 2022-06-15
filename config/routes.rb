cdf2e42164f030e5ce62a59c5fb1e9b90ec0fa23
Emily Johnson <emelizajo22@gmail.com>
2022-06-15 12:15:26 -0700

    change comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   get 'home/about'
 
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
